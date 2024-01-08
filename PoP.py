351ce9a13c246f61c88f8a65e28edb089a9c8a42
brick <djc939@gmail.com>
2024-01-08 13:54:25 -0600

    Update PoP.py

--- PoP.py
+++ PoP.py
@@ -121,7 +121,7 @@ if purp > 0:
     print("")
 
 if glue > 0:
-    print(f"You need {glue} Glue, needing {gumdrops} gumdrops")
+    print(f"You need {glue} Glue, needing {gumdrops} more gumdrops")
     print(f"If you do glue dispenser every day, youll get this in at most {math.ceil(glue/5*22/24)} days")
     tickets = math.ceil((glue*50)/3)
     print(f"You can buy that with {tickets} tickets or {gumdrops*3} Strawberries, Blueberries, and Pineapples TOTAL")
